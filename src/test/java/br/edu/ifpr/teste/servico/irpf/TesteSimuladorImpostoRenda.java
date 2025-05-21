62c89747c1fb767bd69440727b0f14b2cbe70af3
nogutiyuka <nogutiyuka@gmail.com>
2025-05-20 23:56:44 -0300

    teste imposto de renda

--- src/test/java/br/edu/ifpr/teste/servico/irpf/TesteSimuladorImpostoRenda.java
+++ src/test/java/br/edu/ifpr/teste/servico/irpf/TesteSimuladorImpostoRenda.java
@@ -1,20 +1,53 @@
 package br.edu.ifpr.teste.servico.irpf;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import imposto.renda.SimuladorImpostoRenda;
 
 public class TesteSimuladorImpostoRenda {
+
     @Test
-    public void exemploTeste() {
-        // Arrange (PREPARAR) - Roy Osherove no livro “The Art of Unit Testing”.
-        // Given (DADO) - BDD (Behavior Driven Development)
+    public void salarioIsento() {
+        double salario = 1876.24;
+        double imposto = SimuladorImpostoRenda.calcularImposto(salario);
+        assertEquals(0.0, imposto);
+    }
 
-        // Act (AGIR)
-        // When (QUANDO)
+    @Test
+    public void salarioSeteMeioPorCento() {
+        double salario = 2417.98;
+        double imposto = SimuladorImpostoRenda.calcularImposto(salario);
+        assertEquals(38.55, imposto);
+    }
 
-        SimuladorImpostoRenda.calcularImposto(0);
-        // Assert (VERIFICAR)
-        // Then (ENTÃO)
+    @Test
+    public void salarioQuinzePorCento() {
+        double salario = 3049.74;
+        double imposto = SimuladorImpostoRenda.calcularImposto(salario);
+        assertEquals(102.66, imposto);
+    }
+
+    @Test
+    public void salarioVinteDoisMeioPorCento() {
+        double salario = 3964.13;
+        double imposto = SimuladorImpostoRenda.calcularImposto(salario);
+        assertEquals(255.80, imposto);
+    }
+
+    @Test
+    public void salarioVinteSeteMeioPorCento() {
+        double salario = 8974.66;
+        double imposto = SimuladorImpostoRenda.calcularImposto(salario);
+        assertEquals(1598.67, imposto);
+    }
+
+    @Test
+    public void salarioNegativoExcecao() {
+        double salario = -1547.21;
+        org.junit.jupiter.api.Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> SimuladorImpostoRenda.calcularImposto(salario)
+        );
     }
 }
